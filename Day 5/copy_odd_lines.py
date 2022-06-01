900f3e0dc8e475d71bfb4a47ea3661d6ef87d940
AbhishekP-Maplelabs <102032532+AbhishekP-Maplelabs@users.noreply.github.com>
2022-06-01 16:18:24 +0530

    Add files via upload

--- Day 5/copy_odd_lines.py
+++ Day 5/copy_odd_lines.py
@@ -1,30 +1,20 @@
-# open file in read mode
-file = open('file.txt', 'r')
-
-# open other file in write mode
-new_file = open('file_1.txt', 'w')
-
-# read the content of the file line by line
-count = file.readlines()
-type(count)
-for i in range(0, len(count)):
-    if i % 2 == 0:
-        new_file.write(count[i])
-    else:
-        pass
-
-# close the file
-new_file.close()
-
-# open file in read mode
-new_file = open('file_1.txt', 'r')
-
-# read the content of the file
-count1 = new_file.read()
-
-# print the content of the file
-print(count1)
-
-# close all files
-file.close()
-new_file.close()
+"""
+    Description: To read contents of a file and copy only the content of odd lines into new file.
+"""
+try:
+    file = open('file.txt', 'r')
+    new_file = open('file_1.txt', 'w')
+    count = file.readlines()
+    for i in range(0, len(count)):
+        if i % 2 == 0:
+            new_file.write(count[i])
+        else:
+            pass
+    new_file.close()
+    new_file = open('file_1.txt', 'r')
+    count1 = new_file.read()
+    print(count1)
+    file.close()
+    new_file.close()
+except Exception as e:
+    print("Enter valid input")
